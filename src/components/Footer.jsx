8d4f28964e2785c3169ec7b9a906fed21dc10ba8
Suraj Dubey <surajdubey@Surajs-MacBook-Pro.local>
2022-12-14 16:44:49 +0530

    footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,14 @@
 import Instagram from "../images/insta.png";
+import Youtube from "../images/youtube.png";
 
 function Footer() {
   return (
     <>
-      <div className="footer bg-[#1A2142] text-white sm:flex justify-around items-center sm:p-10 p-6 mb-2">
-        <div className="text-3xl mb-4 footer-logo">michen.</div>
+      <div className="footer bg-[#1A2142] text-white flex  items-center sm:p-4 p-6 mb-5">
        
+      <div className="my-2">
+        <a href="https://www.instagram.com/michen_0/" target="_blank" rel="noopener noreferrer"><img src={Youtube} width={60} alt="" /></a>
+        </div>
         <div className="my-2">
         <a href="https://www.instagram.com/michen_0/" target="_blank" rel="noopener noreferrer"><img src={Instagram} width={50} alt="" /></a>
         </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
         <nav className="flex justify-between items-center bg-[#1A2142] text-white p-2 sm:py-5 drop-shadow-md px-6 sm:px-40 fixed top-0 z-40 w-full sm:w-full font">
             <div className="logo font-bold text-2xl 
         ">
-                Michen.
+                michen.
             </div>
             <div className="contact">
                 <div className="p-num">
