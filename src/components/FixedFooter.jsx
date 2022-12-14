05d0e574718d87c4118e6869dacfef2e5124c30e
Suraj Dubey <surajdubey@Surajs-MacBook-Pro.local>
2022-12-14 16:57:46 +0530

    footer updated

--- src/components/FixedFooter.jsx
+++ src/components/FixedFooter.jsx
@@ -1,6 +1,6 @@
 function FixedFooter() {
     return ( <>
-    <div className="fixedfooter  bg-[#1A2142] text-white flex justify-center items-center sm:p-1 p-1 fixed bottom-0 w-full p-5 text-sm">
+    <div className="fixedfooter  bg-[#1A2142] text-white flex justify-center items-center sm:p-1  fixed bottom-0 w-full p-4 text-sm">
     Experience this new comfort<span className="text-[#EEC843] px-2">free</span>for first month.
     </div>
     </> );
