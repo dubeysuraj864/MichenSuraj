c90df0e6982f49ed075534b9dfb796a768f242f9
Suraj Dubey <surajdubey@Surajs-MacBook-Pro.local>
2022-12-15 15:57:28 +0530

    image compressed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,19 +7,13 @@ function Hero() {
       <div className="hero w-full flex justify-center px-5 ">
         <div className="container sm:flex sm:justify-around sm:items-center bg-[#1A2142] text-white sm:pt-10 sm:px-5 sm:py-10 rounded-b-full sm:rounded-b-3xl w-full h-fit sm:h-auto">
           <div className="left ">
-            <div data-aos="fade-zoom-in"
-     data-aos-easing="ease-in-back"
-     data-aos-delay="300"
-     data-aos-offset="0" className="text text-center py-10 sm:py-20 sm:text-left text-[30px] sm:text-[50px]">
+            <div  className="text text-center py-10 sm:py-20 sm:text-left text-[30px] sm:text-[50px]">
               One stop <br /> solution for all your <br />{" "}
               <span className="text-[#F6D54B] font-bold">kitchen</span> hassle.
             </div>
           </div>
           <div className="right relative sm:w-auto w-full h-fit">
-            <div data-aos="fade-left"
-     data-aos-anchor="#example-anchor"
-     data-aos-offset="500"
-     data-aos-duration="500"
+            <div 
               className="images flex justify-center m-auto items-center py-10 sm:p-10  z-10 
             w-fit sm:w-[309px] h-[320px] sm:h-[309px] "
             >
