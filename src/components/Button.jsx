6e9ae1ffc92ac97364e46e9b881d809b0ed96d78
Suraj Dubey <surajdubey@Surajs-MacBook-Pro.local>
2022-12-15 16:20:25 +0530

    fixes

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,10 +2,11 @@ function Button(props) {
   return (
     <>
       <div className="btn  mt-10 " >
-        <button className="  bg-[#1A2142]  text-white px-6 py-3 rounded-md drop-shadow-md hover:bg-white hover:text-[#182144]  ease-in-out duration-500 font-bold hover:drop-shadow-none transition-all text-lg mt-10">
-          {" "}
-          <a href="#rectangle" rel="noopener noreferrer">{props.name}</a>
-        </button>
+       
+        <a href="#rectangle" rel="noopener noreferrer"> <button className="  bg-[#1A2142]  text-white px-6 py-3 rounded-md drop-shadow-md hover:bg-white hover:text-[#182144]  ease-in-out duration-500 font-bold hover:drop-shadow-none transition-all text-lg mt-10">
+        {props.name}
+   
+        </button></a>
       </div>
     </>
   );
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,14 +4,14 @@ import Youtube from "../images/youtube.webp";
 function Footer() {
   return (
     <>
-      <div className="footer bg-[#1A2142] text-white flex  items-center sm:p-4 p-6 sm:mb-6 mb-8">
+      <div className="footer bg-[#1A2142] text-white flex  items-center sm:p-4 p-6 sm:mb-6 mb-9">
         <div className="my-2">
           <a
             href="https://www.instagram.com/michen_0/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <img src={Youtube} width={60} alt="" />
+            <img src={Youtube} width={40} alt="" />
           </a>
         </div>
         <div className="my-2">
@@ -20,7 +20,7 @@ function Footer() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            <img src={Instagram} width={50} alt="" />
+            <img src={Instagram} width={40} alt="" />
           </a>
         </div>
       </div>
